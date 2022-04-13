63da25ff216d933dcd9110429fb1dd00f993e9b0
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-04-12 22:30:31 -0300

    Programacion con funciones

---
+++ main.js
@@ -0,0 +1,59 @@
+//FUNCIONES
+/*
+Temas a tratar:
+-Concepto de funcion
+-Parametros de entrada y salida de una funcion
+-Que es el Scope y cuales son las variables locales 
+y globales
+-Diferencia entre una funcion anonima y una funcion
+flecha 
+*/
+
+//---DEFINICION DE FUNCION---//
+/*
+Una funcion es un conjunto de instrucciones que se
+agrupan para realizar una tarea concreta, que luego
+se puede reutilizar a lo largo de diferentes instancias
+de codigo
+*/
+
+//---DECLARACION---//
+/*
+Se declara a traves de la palabra reservada function.
+Deben tener un nombre en minuscula y sin espacios 
+seguidos de los caracteristicos parentesis (). El 
+contenido de la funcion se escribe entre las llaves.
+El nombre de la funcion no se puede repetir
+*/
+
+//---LLAMADO---//
+/*
+Una vez declarada la funcion, podemos usarla en cualquier
+otra parte del codigo todas las veces que queramos. Para 
+ejecutar la funcion solo hay que escribir su nombre y finalizar 
+la sentencia con un parentesis. A esto se le conoce como 
+llamado a la funcion.
+*/
+//EJEMPLO
+/*
+//Creacion de la funcion
+function saludar(){
+    console.log("Hola" + "\n")
+}
+//LLamado de la funcion 
+saludar()
+saludar()
+saludar()
+saludar()
+*/
+
+//---PARAMETROS---//
+/*
+Cuando enviamos a la funcion uno o mas valores
+para ser empleados en sus operaciones, estamos
+hablando de los parametros de una funcion. Los
+parametros se envian a la funcion mediante variables
+y se colocan entre los parentesis posteriores al 
+nombre de la funcion
+*/
+//15000
